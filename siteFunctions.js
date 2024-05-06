77940148839b8d7d28bc92ee956b568d4177a151
A-Mazeing <108584910+A-Mazeing@users.noreply.github.com>
2024-05-06 21:33:19 +0200

    TestCam

--- siteFunctions.js
+++
@@ -1,8 +0,0 @@
-function BildschirmSwitch() 
-{
-    var startseite = document.getElementById('startseite');
-    var container = document.querySelector('.container');
-
-    startseite.style.display = 'none';
-    container.style.display = 'flex';
-}
